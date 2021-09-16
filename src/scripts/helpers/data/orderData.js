1b0c3f5a2414281cc0f169eb86fa2083c07fb136
unknown <lukus2013@yahoo.com>
2021-09-15 20:09:45 -0500

    3rd push

--- src/scripts/helpers/data/orderData.js
+++ src/scripts/helpers/data/orderData.js
@@ -1,8 +1,57 @@
-// import axios from "axios";
-// import firebaseConfig from "../../../api/apiKeys";
+import axios from 'axios';
+import firebaseConfig from '../../../api/apiKeys';
 
-// const dbUrl = firebaseConfig.databaseURL;
+const dbUrl = firebaseConfig.databaseURL;
 
-// const getOrders = () => new Promise ((resolve, reject) => {
-//     axios.get(`${dbUrl}/`)
-// })
+const getOrders = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/order.json`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
+
+const deleteOrder = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/order/${firebaseKey}.json`)
+    .then(() => getOrders().then((response) => resolve(response)))
+    .catch((error) => reject(error));
+});
+
+const getOneOrder = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/order/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
+
+const createOrder = (orderObject) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/oder.json`, orderObject)
+    .then((response) => {
+      const body = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/order/${response.data.name}.json`, body)
+        .then(() => {
+          getOrders().then(resolve);
+        });
+    }).catch(reject);
+});
+
+const updateOrder = (orderObject) => new Promise((resolve, reject) => {
+  axios.patch(`${dbUrl}/order/${orderObject.firebaseKey}.json`, orderObject)
+    .then(() => getOrders().then(resolve))
+    .catch(reject);
+});
+
+const getOrderItems = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/order.json?orderBy="orderId"&equalTo="${firebaseKey}"`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch(reject);
+});
+
+const deleteOrderItems = (orderId) => new Promise((resolve, reject) => {
+  getOrderItems(orderId).then((orderItemArray) => {
+    const deleteItems = orderItemArray.map((item) => deleteItems(item.firebaseKey));
+
+    Promise.all([...deleteItems]).then(() => resolve(deleteOrder(orderId)));
+  }).catch(reject);
+});
+
+export {
+  getOrders, deleteOrder, getOneOrder, createOrder, updateOrder, getOrderItems, deleteOrderItems
+};
